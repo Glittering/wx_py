726d2472807f8952f51605d50df9cbee2eeed766
Glittering <zhaoshichengzhao@sina.com>
2017-10-09 14:56:40 +0800

    static

--- wx_py/settings.py
+++ wx_py/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'wx_py.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates').replace('\\', '/')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+MEDIA_URL = '/upload/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'upload').replace("//", "/")
